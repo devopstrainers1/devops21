6d936c882bba568fb08d33747aeb4ffbf1577868
ajay <demo@demo.local>
2020-11-28 12:47:27 +0530

    done

--- demo.php
+++ demo.php
@@ -6,3 +6,4 @@ This is code fix done by user zook
 This is fixed on bugfix branch
 This is from demo branch
 This is new line added on bugfix
+This is demo code for php code on demo for custimer xyz
